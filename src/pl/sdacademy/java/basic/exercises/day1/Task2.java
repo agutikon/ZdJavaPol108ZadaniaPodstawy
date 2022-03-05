cdce099cc1609930c764592c9f193c0722154e61
agata <konieczkaagata@gmail.com>
2022-03-05 11:40:48 +0100

    initial commit

---
+++ src/pl/sdacademy/java/basic/exercises/day1/Task2.java
@@ -0,0 +1,12 @@
+package pl.sdacademy.java.basic.exercises.day1;
+
+import java.util.Scanner;
+
+public class Task2 {
+    final double PI = Math.PI;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        }
+    }
+
